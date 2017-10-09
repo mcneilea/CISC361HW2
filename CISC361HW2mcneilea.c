c036dac354ce6147124366ebf7fe20674d120a64
Emily McNeil <mcneilea@udel.edu>
2017-10-09 16:07:39 -0400

    WHOOPS I'm an idiot

--- CISC361HW2mcneilea.c
+++ CISC361HW2mcneilea.c
@@ -4,14 +4,57 @@
 
 int AVG, MAX, MIN, NUMARGS;
 
+void *average(void *param);
+void *maximum(void *param);
+void *minimum(void *param);
+
+int main(int argc, char *argv[])
+{
+
+    pthread_t tidAVG;
+    pthread_t tidMAX;
+    pthread_t tidMIN;
+    NUMARGS = argc - 1;
+
+    if (NUMARGS < 1)
+    {
+        printf("No data provided. Exiting. \n");
+        return 0;
+    }
+
+    int *array = malloc(NUMARGS * sizeof(int));
+
+    for(int i = 0; i < NUMARGS; i++){
+      array[i] = atoi(argv[i+1]);
+  //    printf("Loading array: %d\n", array[i]);
+    }
+
+    pthread_create(&tidAVG, 0, average, array);
+    pthread_create(&tidMAX, 0, maximum, array);
+    pthread_create(&tidMIN, 0, minimum, array);
+
+    pthread_join(tidAVG, NULL);
+    pthread_join(tidMAX, NULL);
+    pthread_join(tidMIN, NULL);
+
+    printf("The average value is %d \n", AVG);
+    printf("The minimum value is %d \n", MIN);
+    printf("The maximum value is %d \n", MAX);
+
+    free(array);
+    return 0;
+}
+
+
 void *average(void *param)
 {
     int sum = 0;
     int *arr = (int *)param;
-
+//    printf("\nConfirming NUMARGS: %d\n", NUMARGS);
     for (int i = 0; i < NUMARGS; i++)
     {
         sum += arr[i];
+  //      printf("Summing array: +%d ", arr[i]);
     }
 
     AVG = sum / NUMARGS;
@@ -20,8 +63,8 @@ void *average(void *param)
 
 void *maximum(void *param)
 {
-    int max = 0;
     int *arr = (int *)param;
+    int max = arr[0];
 
     for (int i = 0; i < NUMARGS; i++)
     {
@@ -37,12 +80,12 @@ void *maximum(void *param)
 
 void *minimum(void *param)
 {
-    int min = 0;
     int *arr = (int *)param;
+    int min = arr[0];
 
     for (int i = 0; i < NUMARGS; i++)
     {
-        if (min < arr[i])
+        if (min > arr[i])
         {
             min = arr[i];
         }
@@ -50,32 +93,4 @@ void *minimum(void *param)
 
     MIN = min;
     pthread_exit(0);
-}
-
-int main(int argc, char *argv[])
-{
-
-    pthread_t tidAVG;
-    pthread_t tidMAX;
-    pthread_t tidMIN;
-    NUMARGS = argc - 1;
-
-    if (NUMARGS < 1)
-    {
-        printf("No data provided. Exiting.");
-        return 0;
-    }
-
-    int *array = malloc(NUMARGS * sizeof(int));
-
-    pthread_create(&tidAVG, 0, average, array);
-    pthread_create(&tidMAX, 0, maximum, array);
-    pthread_create(&tidMIN, 0, minimum, array);
-
-    printf("The average value is %f", AVG);
-    printf("The average value is %f", MIN);
-    printf("The average value is %f", MAX);
-
-    free(array);
-    return 0;
 }
--- Makefile
+++ Makefile
@@ -1,19 +1,19 @@
 all : testEX testSAME testINCR testEMPTY
 
-361hw2.exe: CISC361HW2mcneilea.c
-	cc -std=c11 -pedantic -o CISC361HW2mcneilea.exe CISC361HW2mcneilea.c -lpthread
+CISC361HW2mcneilea.exe: CISC361HW2mcneilea.c
+	cc -pthread -std=c11 -pedantic -o CISC361HW2mcneilea.exe CISC361HW2mcneilea.c
 
-testEX : CISC361HW2mcneilea.c
-	./CISC361HW2mcneilea.c 90 81 78 95 79 72 85
+testEX : CISC361HW2mcneilea.c Makefile
+	./CISC361HW2mcneilea.exe 90 81 78 95 79 72 85
 
-testSAME : CISC361HW2mcneilea.c
-	./CISC361HW2mcneilea.c 5 5 5 5 5
+testSAME : CISC361HW2mcneilea.c Makefile
+	./CISC361HW2mcneilea.exe 5 5 5 5 5
 
 testINCR : CISC361HW2mcneilea.c
-	./CISC361HW2mcneilea.c 1 2 3 4 5 6 7 8 9 10
+	./CISC361HW2mcneilea.exe 1 2 3 4 5 6 7 8 9 10
 
 testEMPTY: CISC361HW2mcneilea.c
-	./CISC361HW2mcneilea.c 
+	./CISC361HW2mcneilea.exe
 
 clean :
 	rm -f *.exe *~ *.o
